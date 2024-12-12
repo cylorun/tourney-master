97c8d7ef5ff0ca032e86b52a820ffa77c6e96111
cylorun <allidabbi@gmail.com>
2024-12-12 00:01:12 +0000

    fix stream manager

--- src/main/java/com/cylorun/gui/StreamManager.java
+++ src/main/java/com/cylorun/gui/StreamManager.java
@@ -5,9 +5,7 @@ import com.cylorun.model.Player;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class StreamManager extends JPanel {
 
@@ -23,9 +21,10 @@ public class StreamManager extends JPanel {
         for (int i = 0; i < rows * cols; i++) {
             Player streamer = ttvNames.size() > i ?  ttvNames.get(i) : prev;
             prev = streamer;
+
             StreamerPanel streamerPanel = new StreamerPanel(streamer, this.ttvNames);
             streamerPanel.onCheckBoxChange(() -> {
-                System.out.println("Click: " + streamerPanel.ttvName);
+                System.out.println("Click: " + streamerPanel.streamer);
                 this.selectedStreamers.add(streamerPanel);
 
                 if (this.selectedStreamers.size() == 2) {
@@ -39,20 +38,20 @@ public class StreamManager extends JPanel {
     }
 
     public void swapPanels(StreamerPanel a, StreamerPanel b) {
-        System.out.println("Swapping: " + a.ttvName + ", " + b.ttvName);
+        System.out.println("Swapping: " + a.streamer + ", " + b.streamer);
     }
 
     public static class StreamerPanel extends JPanel {
-        private Player ttvName;
+        private Player streamer;
         private Runnable onCheckBoxChange;
 
         public StreamerPanel(Player streamer, List<Player> ttvNames) {
             super(new BorderLayout());
-            this.ttvName = streamer;
+            this.streamer = streamer;
 
             JComboBox<String> streamerDropdown = new JComboBox<>(ttvNames.stream().map(p -> p.twitch).toArray(String[]::new));
+            streamerDropdown.setSelectedItem(streamer.twitch);
 
-            streamerDropdown.setSelectedItem(streamer);
             this.add(streamerDropdown, BorderLayout.CENTER);
 
             JCheckBox activeCheck = new JCheckBox();
--- src/main/java/com/cylorun/model/Player.java
+++ src/main/java/com/cylorun/model/Player.java
@@ -5,7 +5,25 @@ public class Player {
     public String ign;
 
     public Player(String twitch, String ign){
-        this.twitch = twitch;
-        this.ign = ign;
+        this.twitch = twitch.trim();
+        this.ign = ign.trim();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Player p) {
+            return p.ign.equalsIgnoreCase(this.ign) &&
+                    p.twitch.equalsIgnoreCase(this.twitch);
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "twitch='" + twitch + '\'' +
+                ", ign='" + ign + '\'' +
+                '}';
     }
 }
